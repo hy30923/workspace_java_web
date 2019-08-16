999782eb301a27ab00fae53715b87602bd886d78
user <user@example.com>
2019-08-16 17:00:44 +0800

    2019/08/16

---
+++ UseDispatch/src/com/abc/userdispatch/UserDispatch.java
@@ -0,0 +1,65 @@
+package com.abc.userdispatch;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class UserDispatch
+ */
+@WebServlet("/UserDispatch")
+public class UserDispatch extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public UserDispatch() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		request.setCharacterEncoding("UTF-8");
+		response.setContentType("text/html; charset=utf-8");
+		
+		PrintWriter writer = response.getWriter();
+		
+		String name = request.getParameter("name");
+		String birthday = request.getParameter("birthday");
+		String gender = request.getParameter("gender");
+		String hobbies[] = request.getParameter("hobbies").split("\\s|\\n|,|¡B|¡A");
+		
+		if(gender.equals("male")) {
+			
+			response.sendRedirect("https://www.pchome.com.tw");
+		}
+		
+		else if(gender.equals("female")){
+			
+			response.sendRedirect("https://tw.yahoo.com/");
+		}
+		
+		else {
+			
+		}
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
